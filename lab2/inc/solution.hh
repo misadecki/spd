409d6dc35266877f7d47da772f07f56cedd46dc2
Wojciech Zielinski <278327@student.pwr.edu.pl>
2026-05-24 21:19:20 +0200

    Add solution Cmax

--- lab2/inc/solution.hh
+++ lab2/inc/solution.hh
@@ -3,15 +3,22 @@
 
 #include "dataTypes.hh"
 #include "Permutation.hh"
-#include <vector>
+#include <alghoritm>
 
 class Solution {
   //int current_L = 0;
   Permutation &p;
-  std::vector<Task> &tasks;
+  Instance &i;
+  std::vector<std::vector<int>> c;
 public:
   int solve();
-  Solution(Permutation &perm, std::vector<Task> &t) : p(perm), tasks(t) {};
+  Solution(Permutation &perm, Instance &instance) : p(perm), i(instance)
+  {
+    c.resize(i->m+1);
+    for(int n=0; n<(i->n+1); n++)
+      c[i].resize(i->n);
+      std::fill(c[i].begin(), c[i].end(), 0);
+  };
 };
 
 #endif // SOLUTION_HH
--- lab2/src/solution.cpp
+++ lab2/src/solution.cpp
@@ -2,17 +2,10 @@
 #include <climits>
 
 int Solution::solve() {
-  int Lmax = INT_MIN;
-  int current_L = 0;
-  int t = 0;
-
-  for(int i = 0; i < p.get_n(); ++i) {
-    t = std::max(t, tasks[p[i]].rj);
-    t += tasks[p[i]].pj;
-    
-    current_L = t - tasks[p[i]].dj;
-    //std::cout << "Task " << i << " -> L_curr: " << current_L << "\t L_max: " << Lmax << std::endl; 
-    if(current_L > Lmax) {Lmax = current_L;}
+  for (int a=1; a<=(i->m); a++){
+	 for(int b=1; b<=(i->n); b++)
+		c[a][b] = std::max(c[a][b-1], c[a-1][b]) + i[p[b]][a];	 
   }
-  return Lmax;
+
+  return c[i->m][i->n];
 }
