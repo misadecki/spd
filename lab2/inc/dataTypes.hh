d5ed722b1f470ae5bab8db2fc3855f3c0a148e1e
Cardaver <michal.sadecki@proton.me>
2026-05-24 21:10:30 +0200

    feat: operator[] for instance

--- lab2/inc/dataTypes.hh
+++ lab2/inc/dataTypes.hh
@@ -1,46 +1,48 @@
 #ifndef DATATYPES_HH
 #define DATATYPES_HH
+
 #include <iostream>
 #include <string>
 #include <sstream>
 
 struct Instance {
-    const int n;	// number of tasks
-    const int m;	// number of machines/operations
-    int **tasks;	// tasks array 
+  const int n;	// number of tasks
+  const int m;	// number of machines/operations
+  int **tasks;	// tasks array 
 
-    Instance(int n_tasks, int m_operations, std::istringstream &input) :
-	    n(n_tasks), m(m_operations) {
-		    std::string buf;
-	int temp;
-	tasks = (int**)malloc(n*sizeof(int*));
-	for (int i=0; i<n; ++i){
-		    tasks[i] = (int*)malloc(m*sizeof(int));
-		    for(int j=0; j<m; ++j){
-			input >> buf;
-			temp = std::stoi(buf);
-			input >> buf;
-			tasks[i][temp] = std::stoi(buf);
-		    }
-	}
+  Instance(int n_tasks, int m_operations, std::istringstream &input) :
+    n(n_tasks), m(m_operations) {
+    std::string buf;
+    int temp;
+    tasks = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; ++i){
+      tasks[i] = (int*)malloc(m * sizeof(int));
+      for (int j = 0; j < m; ++j){
+        input >> buf;
+        temp = std::stoi(buf);
+        input >> buf;
+        tasks[i][temp] = std::stoi(buf);
+      }
     }
+  }
 
-    ~Instance(){
-	    for(int i=0; i<n; ++i){
-		    free(tasks[i]);
-	    }
+  int* operator[](int idx) { return tasks[idx]; }
+  ~Instance() {
+    for (int i = 0; i < n; ++i){
+      free(tasks[i]);
     }
+  }
 
-    void print() {
-	    std::cout << "Number of tasks: " << n << std::endl;
-	    std::cout << "Number of operations: " << m << std::endl;
-	   for(int i=0; i<n; ++i){
-		   std::cout << "Task " << i << ": ";
-		   for(int j=0; j<m; ++j)
-			   std::cout << j << "(" << tasks[i][j] << ") ";
-		   std::cout << std::endl;
-	   } 
-    }
+  void print() {
+    std::cout << "Number of tasks: " << n << std::endl;
+    std::cout << "Number of operations: " << m << std::endl;
+    for (int i = 0; i < n; ++i){
+      std::cout << "Task " << i << ": ";
+      for (int j = 0; j < m; ++j)
+        std::cout << j << "(" << tasks[i][j] << ") ";
+      std::cout << std::endl;
+    } 
+  }
 };
 
 #endif // DATATYPES_HH
