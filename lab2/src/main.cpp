24ee068752b3d4aa14b983a6d25107fe642bb8e5
Wojciech Zielinski <278327@student.pwr.edu.pl>
2026-06-04 15:00:09 +0200

    Fix argc check

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -7,6 +7,10 @@
 #include <cstring>
 
 int main(int argc, char *argv[]) {
+    if(argc < 3){
+	    std::cout << "Usage: " << argv[0] << " <plik_testowy>" << " <brute|johnson|neh|fneh|binary>"<< std::endl;
+	    return 0;
+    }
     std::ostringstream dane;
     std::string file(argv[1]);
     importData(file, dane);
@@ -14,21 +18,15 @@ int main(int argc, char *argv[]) {
     i->print();
     Problem problem(*i, i->n);
 
-    if(argc < 3){
-	    std::cout << "Usage: " << argv[0] << " <plik_testowy>" << " <_ |brute|johnson|neh| _ | _>"<< std::endl;
-	    return 0;
-    }
-    if(!strcmp(argv[2], "own")) {
-    	//problem.own_algorithm();
-    } else if (!strcmp(argv[2], "brute")) {
+    if (!strcmp(argv[2], "brute")) {
     	problem.brute_force();
     } else if (!strcmp(argv[2], "johnson")) {
     	problem.johnson();
     } else if (!strcmp(argv[2], "neh")) {
 	problem.NEH();
-    } else if (!strcmp(argv[2], "schrage")) {
-   	//problem.Schrage();
-    } else if (!strcmp(argv[2], "p_schrage")) {
+    } else if (!strcmp(argv[2], "fneh")) {
+   	problem.FNEH();
+    } else if (!strcmp(argv[2], "binary")) {
     	//problem.Schrage_preemptive();
     }
     std::cout << argv[1] << std::endl;
